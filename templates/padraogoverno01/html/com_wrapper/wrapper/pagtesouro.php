0fefe82caa608c58f7f69bdbcaf971ed88d4a4fd
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-02-17 22:00:50 -0300

    Define sobrescrita básica do componente wrapper

---
+++ templates/padraogoverno01/html/com_wrapper/wrapper/pagtesouro.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Components.Wrapper
+ *
+ * @author      Rene Bentes Pinto <http://github.com/renebentes>
+ * @copyright   Copyright (C) 2021 Rene Bentes Pinto, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * @link        http://github.com/renebentes
+ * @since       0.1.0
+ */
+
+// No direct access.
+defined('_JEXEC') or die('Restricted access!'); ?>
+
+<form action="" method="post">
+	<div class="row">
+		<div class="control-group span4">
+			<label for="ug" class="control-label">UG</label>
+			<div class="controls"><input class="uneditable-input" type="text" ></div>
+		</div>
+		<div class="control-group span4">
+			<label for="codigoServico" class="control-label">Código do Serviço</label>
+			<div class="controls"><input type="text"></div>
+		</div>
+		<div class="control-group span4">
+			<label for="numeroReferencia" class="control-label">Número de Referência</label>
+			<div class="controls"><input type="text"></div>
+		</div>
+	</div>
+</form>
